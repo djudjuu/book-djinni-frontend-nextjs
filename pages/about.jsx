5435436187c769cedf17825eaf15b1daa203ae09
djudjuu <julfaber@gmail.com>
2022-06-29 15:46:52 +0200

    simple header in layout page + but page

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+import Layout from "components/Layout";
+
+// component that renders a simple div
+const About = () => (
+  <Layout>
+    <h1>About</h1>
+    <p>This is the about page</p>
+  </Layout>
+);
+
+export default About;
