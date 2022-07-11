c5823cdc6b94528fc7847e6eb35b2afb722791e2
djudjuu <julfaber@gmail.com>
2022-07-11 13:26:36 +0200

    libgenlink

--- components/BookCard.jsx
+++ components/BookCard.jsx
@@ -1,9 +1,15 @@
 const BookCard = ({ book }) => {
+  const title = book.title.replace(/\s/g, "+");
+  const author = book.author.replace(/\s/g, "+");
+  const libgenlink = `https://libgen.is/search.php?req=${title}+${author}`;
   return (
     <div className="book-card">
       <h3>{book.title}</h3>
       <p> by {book.author}</p>
       {/* <p>isbn: {book.isbn}</p> */}
+      <a href={libgenlink} target="_blank">
+        <button>View on Libgen</button>
+      </a>
     </div>
   );
 };
